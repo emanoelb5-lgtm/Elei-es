77bee14731b0253d6d1022cf26d48059209f6753
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 20:06:59 -0300

    build: prepara v0.7.1 com persistencia temporal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.7.0"
+        versionCode = 14
+        versionName = "0.7.1"
     }
 
     buildTypes {
