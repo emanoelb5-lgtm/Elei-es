9fe1700cedafbc23437a6d822258259c5fa92756
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 14:12:57 -0300

    build: prepara v0.3.0 com analytics auditaveis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.1"
+        versionCode = 5
+        versionName = "0.3.0"
     }
 
     buildTypes {
