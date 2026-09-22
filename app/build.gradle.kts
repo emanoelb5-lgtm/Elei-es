fbe4ddba7d9c07616d00ef5b468c33bbd342bcef
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 14:32:54 -0300

    build: prepara v0.4.0 de calibracao e diagnostico

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.0"
+        versionCode = 6
+        versionName = "0.4.0"
     }
 
     buildTypes {
