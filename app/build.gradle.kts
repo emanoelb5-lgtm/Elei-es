a5f00281d17766ed1e2b769cf7a2174b0d6ab25f
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 21:21:00 -0300

    build: prepara v0.8.1 com frescor temporal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "0.8.0"
+        versionCode = 16
+        versionName = "0.8.1"
     }
 
     buildTypes {
