682f178dd9a46c8ef6cc040d283a1082d1689227
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-23 10:12:44 -0300

    build: prepara v0.9.4 com incerteza parametrica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "0.9.3"
+        versionCode = 21
+        versionName = "0.9.4"
     }
 
     buildTypes {
