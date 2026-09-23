5082c887cb53f216b9d81c28d5c3291b9f2e99c5
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 21:48:24 -0300

    build: prepara v0.9.1 com cobertura de cenarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.9.0"
+        versionCode = 18
+        versionName = "0.9.1"
     }
 
     buildTypes {
