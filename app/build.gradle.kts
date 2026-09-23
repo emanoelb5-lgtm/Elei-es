f2b27470f7a1136b8d558a76fc414a631f27c23d
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 21:10:35 -0300

    build: prepara v0.8.0 com diversidade metodologica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.7.1"
+        versionCode = 15
+        versionName = "0.8.0"
     }
 
     buildTypes {
