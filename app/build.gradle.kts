3ce75b1786a6fe5811b465a4781dde2d7994f9b1
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-23 07:54:55 -0300

    build: prepara v0.9.3 com stress test dos pesos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.9.2"
+        versionCode = 20
+        versionName = "0.9.3"
     }
 
     buildTypes {
