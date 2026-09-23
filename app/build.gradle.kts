889cb61fb715cc950b04028272c63acc43be66ef
emanoelb5-lgtm <emanoelb5@gmail.com>
2026-09-22 21:38:46 -0300

    build: prepara v0.9.0 com house effect em sombra

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "br.com.eleicoes.termometro"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.8.1"
+        versionCode = 17
+        versionName = "0.9.0"
     }
 
     buildTypes {
